5313f14382e3a7e27cf418a568a7edb768305f02
Lucas da Silva <lucas2005sf@gmail.com>
2024-04-30 21:53:40 -0300

    Casting-java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+       double numerodouble = 15.9;
+       int numerointeiro = (int) numerodouble;
+        System.out.println("Casting de double para int: " + numerointeiro);
+    }
+}
